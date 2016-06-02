d4795ad67eaaa3149b1a202a99186d34f44120ff
dougrchamberlain <dougrchamberlain@gmail.com>
2016-06-01 21:35:39 -0400

    this patch adds files that weren't pushed previously

---
+++ server.js
@@ -0,0 +1,34 @@
+var express = require("express");
+var path = require("path");
+var bodyParser = require("body-parser");
+var mongodb = require("mongodb");
+
+var app = express();
+
+app.use(express.static(__dirname + "/app"));
+app.use(bodyParser.json());
+
+
+var db;
+
+mongodb.MongoClient.connect(process.env.MONGODB_URI, function (err, database){
+    if(err){
+        console.log(err);
+        process.exit(1);
+    }
+});
+
+var port = process.env.port || 8080;
+
+
+function handleError(res, reason, message, code){
+    console.log("ERROR: " + reason);
+    res.status(code || 500).json({"error": message});
+}
+
+var server = app.listen(port, function(){
+    var port = server.address().port;
+    console.log(server.address());
+    console.log("http://localhost:" + port )
+});
+
